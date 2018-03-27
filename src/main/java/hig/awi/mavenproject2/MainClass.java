8ec66f5debb44dc84214a288d1049e6e37ae6a03
awi <awi@13470-L.HIG-AD.hig.se>
2018-03-27 09:29:26 +0200

    Initial commit

---
+++ src/main/java/hig/awi/mavenproject2/MainClass.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hig.awi.mavenproject2;
+
+/**
+ *
+ * @author awi
+ */
+public class MainClass {
+    
+    int i = 4;
+}
